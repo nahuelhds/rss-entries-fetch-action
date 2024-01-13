3ddd9c44f40b44ac58f0204852e58c416fc08567
Nahue <nahuelhds@gmail.com>
2024-01-13 00:47:53 -0300

    Tests adaptations

--- src/utils/fs.test.ts
+++ src/utils/fs.test.ts
@@ -4,62 +4,60 @@ import { FileExistsError, storeFile } from "./fs";
 
 jest.mock("fs");
 
-describe("fs", () => {
-  describe("storeFile", () => {
-    it("throws FileExistsError if file already exists", () => {
-      fs.existsSync = jest.fn().mockReturnValue(true);
-      expect(() => storeFile("some-file.json", "fileContents")).toThrow(
-        FileExistsError,
-      );
+describe("storeFile", () => {
+  it("throws FileExistsError if file already exists", () => {
+    fs.existsSync = jest.fn().mockReturnValue(true);
+    expect(() => storeFile("some-file.json", "fileContents")).toThrow(
+      FileExistsError,
+    );
+  });
+
+  it("creates the file", () => {
+    const expectedPath = `dir/file.json`;
+    const expectedContent = "fileContents";
+    fs.existsSync = jest.fn();
+    fs.mkdirSync = jest.fn();
+    fs.writeFileSync = jest.fn();
+
+    storeFile(expectedPath, expectedContent);
+    expect(fs.writeFileSync).toHaveBeenCalledWith(
+      expectedPath,
+      expectedContent,
+      { encoding: "utf8" },
+    );
+  });
+
+  it("creates the dir if it does not exists", () => {
+    const expectedDir = "dir";
+    const path = `${expectedDir}/file.json`;
+    fs.existsSync = jest
+      .fn()
+      // File exists
+      .mockReturnValueOnce(false)
+      // Dir exists
+      .mockReturnValueOnce(false);
+
+    fs.mkdirSync = jest.fn();
+
+    storeFile(path, "fileContents");
+    expect(fs.mkdirSync).toHaveBeenCalledWith(expectedDir, {
+      recursive: true,
     });
+  });
 
-    it("creates the file", () => {
-      const expectedPath = `dir/file.json`;
-      const expectedContent = "fileContents";
-      fs.existsSync = jest.fn();
-      fs.mkdirSync = jest.fn();
-      fs.writeFileSync = jest.fn();
+  it("does not created the dir if it exists already", () => {
+    const expectedDir = "dir";
+    const path = `${expectedDir}/file.json`;
+    fs.existsSync = jest
+      .fn()
+      // File exists
+      .mockReturnValueOnce(false)
+      // Dir exists
+      .mockReturnValueOnce(true);
 
-      storeFile(expectedPath, expectedContent);
-      expect(fs.writeFileSync).toHaveBeenCalledWith(
-        expectedPath,
-        expectedContent,
-        { encoding: "utf8" },
-      );
-    });
+    fs.mkdirSync = jest.fn();
 
-    it("creates the dir if it does not exists", () => {
-      const expectedDir = "dir";
-      const path = `${expectedDir}/file.json`;
-      fs.existsSync = jest
-        .fn()
-        // File exists
-        .mockReturnValueOnce(false)
-        // Dir exists
-        .mockReturnValueOnce(false);
-
-      fs.mkdirSync = jest.fn();
-
-      storeFile(path, "fileContents");
-      expect(fs.mkdirSync).toHaveBeenCalledWith(expectedDir, {
-        recursive: true,
-      });
-    });
-
-    it("does not created the dir if it exists already", () => {
-      const expectedDir = "dir";
-      const path = `${expectedDir}/file.json`;
-      fs.existsSync = jest
-        .fn()
-        // File exists
-        .mockReturnValueOnce(false)
-        // Dir exists
-        .mockReturnValueOnce(true);
-
-      fs.mkdirSync = jest.fn();
-
-      storeFile(path, "fileContents");
-      expect(fs.mkdirSync).not.toHaveBeenCalled();
-    });
+    storeFile(path, "fileContents");
+    expect(fs.mkdirSync).not.toHaveBeenCalled();
   });
 });
--- src/utils/io.test.ts
+++ src/utils/io.test.ts
@@ -3,72 +3,67 @@ import { buildFilename, getDestinationFolder, getInputFeedUrls } from "./io";
 
 const originalEnv = process.env;
 
-describe("io", () => {
-  beforeEach(() => {
-    process.env = { ...originalEnv };
+beforeEach(() => {
+  process.env = { ...originalEnv };
+});
+
+describe("getInputFeedUrls", () => {
+  it("throws NoUrlsGivenError if FEED_URLS is an empty string", () => {
+    process.env.FEED_URLS = "";
+    expect(() => getInputFeedUrls()).toThrow(NoUrlsGivenError);
   });
 
-  describe("getInputFeedUrls", () => {
-    it("throws NoUrlsGivenError if FEED_URLS is an empty string", () => {
-      process.env.FEED_URLS = "";
-      expect(() => getInputFeedUrls()).toThrow(NoUrlsGivenError);
-    });
-
-    it("throws InvalidUrlError if FEED_URLS is not an URL", () => {
-      process.env.FEED_URLS = "not-a-val;;;id-url";
-      expect(() => getInputFeedUrls()).toThrow(InvalidUrlError);
-    });
-
-    it("throws InvalidUrlError if FEED_URLS contains a not an URL", () => {
-      process.env.FEED_URLS =
-        '["https://www.google.com", "not-a-val;;;id-url"]';
-      expect(() => getInputFeedUrls()).toThrow(InvalidUrlError);
-    });
-
-    it("throws ParseUrlsError if FEED_URLS is not an array or string", () => {
-      process.env.FEED_URLS =
-        '{ "https://www.google.com": "not-a-val;;;id-url" }';
-      expect(() => getInputFeedUrls()).toThrow(ParseUrlsError);
-    });
-
-    it("returns URL per every url string given", () => {
-      const urls = ["https://www.google.com", "https://www.facebook.com"];
-      process.env.FEED_URLS = JSON.stringify(urls);
-      expect(getInputFeedUrls()).toStrictEqual(urls.map((url) => new URL(url)));
-    });
-
-    it("returns an URL array when a common string is given", () => {
-      const url = "https://www.google.com";
-      process.env.FEED_URLS = JSON.stringify(url);
-      expect(getInputFeedUrls()).toStrictEqual([new URL(url)]);
-    });
+  it("throws InvalidUrlError if FEED_URLS is not an URL", () => {
+    process.env.FEED_URLS = "not-a-val;;;id-url";
+    expect(() => getInputFeedUrls()).toThrow(InvalidUrlError);
   });
 
-  describe("getOutputDir", () => {
-    it.each([[undefined], [""]])(
-      "returns ./ if DESTINATION_FOLDER is %s",
-      (falsyValue) => {
-        process.env.DESTINATION_FOLDER = falsyValue;
-        expect(getDestinationFolder()).toBe("./");
-      },
+  it("throws InvalidUrlError if FEED_URLS contains a not an URL", () => {
+    process.env.FEED_URLS = '["https://www.google.com", "not-a-val;;;id-url"]';
+    expect(() => getInputFeedUrls()).toThrow(InvalidUrlError);
+  });
+
+  it("throws ParseUrlsError if FEED_URLS is not an array or string", () => {
+    process.env.FEED_URLS =
+      '{ "https://www.google.com": "not-a-val;;;id-url" }';
+    expect(() => getInputFeedUrls()).toThrow(ParseUrlsError);
+  });
+
+  it("returns URL per every url string given", () => {
+    const urls = ["https://www.google.com", "https://www.facebook.com"];
+    process.env.FEED_URLS = JSON.stringify(urls);
+    expect(getInputFeedUrls()).toStrictEqual(urls.map((url) => new URL(url)));
+  });
+
+  it("returns an URL array when a common string is given", () => {
+    const url = "https://www.google.com";
+    process.env.FEED_URLS = JSON.stringify(url);
+    expect(getInputFeedUrls()).toStrictEqual([new URL(url)]);
+  });
+});
+
+describe("getOutputDir", () => {
+  it.each([[undefined], [""]])(
+    "returns ./ if DESTINATION_FOLDER is %s",
+    (falsyValue) => {
+      process.env.DESTINATION_FOLDER = falsyValue;
+      expect(getDestinationFolder()).toBe("./");
+    },
+  );
+
+  it("returns DESTINATION_FOLDER if it is set", () => {
+    process.env.DESTINATION_FOLDER = "./public";
+    expect(getDestinationFolder()).toBe("./public");
+  });
+});
+
+describe("buildFilename", () => {
+  it('hashes with "shake256" algorithm and "base64url" digest', () => {
+    expect(buildFilename("https://www.google.com")).toBe(
+      "Bt0Lyb9EIA2yt8uFZWTd0saWhOXHx9illE_1E8hcfxh9h5qvVbd0Yg",
     );
-
-    it("returns DESTINATION_FOLDER if it is set", () => {
-      process.env.DESTINATION_FOLDER = "./public";
-      expect(getDestinationFolder()).toBe("./public");
-    });
   });
-
-  describe("buildFilename", () => {
-    it('hashes with "shake256" algorithm and "base64url" digest', () => {
-      expect(buildFilename("https://www.google.com")).toBe(
-        "Bt0Lyb9EIA2yt8uFZWTd0saWhOXHx9illE_1E8hcfxh9h5qvVbd0Yg",
-      );
-    });
-    it("cuts the hash in the defined longitude", () => {
-      expect(buildFilename("https://www.google.com", 10)).toBe(
-        "Bt0Lyb9EIA2ytw",
-      );
-    });
+  it("cuts the hash in the defined longitude", () => {
+    expect(buildFilename("https://www.google.com", 10)).toBe("Bt0Lyb9EIA2ytw");
   });
 });
