10d842203280d7b8f52da356b3a08b3daec6767d
Nahue <nahuelhds@gmail.com>
2024-01-12 21:09:47 -0300

    Utils tests

--- src/logger.ts
+++ src/logger.ts
@@ -10,25 +10,10 @@ export const logger = createLogger({
     format.splat(),
     format.json(),
   ),
-  defaultMeta: { service: "diffnews" },
+  defaultMeta: { service: "diffnews-entries-fetch" },
   transports: [
-    //
-    // - Write to all logs with level `info` and below to `quick-start-combined.log`.
-    // - Write all logs error (and below) to `quick-start-error.log`.
-    //
-    new transports.File({ filename: "diffnews-error.log", level: "error" }),
-    new transports.File({ filename: "diffnews-combined.log" }),
-  ],
-});
-
-//
-// If we're not in production then **ALSO** log to the `console`
-// with the colorized simple format.
-//
-if (process.env.NODE_ENV !== "production") {
-  logger.add(
     new transports.Console({
       format: format.combine(format.colorize(), format.simple()),
     }),
-  );
-}
+  ],
+});
